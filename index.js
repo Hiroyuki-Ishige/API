ddb3cc54d02951bf08f64887991c6532087e831d
hiroyuki_ishige <hiroyukiishige@hotmail.com>
2024-07-11 07:15:12 +0900

    test for git repository name change

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res,)=>{    
-    res.render("index.ejs", {sample: "changed folder name"})
+    res.render("index.ejs", {sample: "changed folder name v2"})
 });
 
 app.listen(port, ()=>{
